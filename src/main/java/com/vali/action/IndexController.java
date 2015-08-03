6e87fbea940424dc38decf31e2e7dc1f6490e6a2
vali.wang <vali.wang@dianping.com>
2015-08-03 19:54:05 +0800

    init project

---
+++ src/main/java/com/vali/action/IndexController.java
@@ -0,0 +1,17 @@
+package com.vali.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/")
+    public ModelAndView listProjects() {
+        return new ModelAndView("index", new HashMap());
+    }
+
+}
