9a5609bbc7212e3325c4c5452f1c2fd1095bf78f
shuai.fan <shuai.fan@dianping.com>
2015-08-25 10:55:45 +0800

    af

--- src/main/java/com/vali/action/setting/YearHolidayInitAction.java
+++ src/main/java/com/vali/action/setting/YearHolidayInitAction.java
@@ -6,6 +6,7 @@ import com.vali.service.setting.YearHolidayInitService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 
@@ -24,8 +25,7 @@ public class YearHolidayInitAction {
     }
 
     @RequestMapping(value = "/sys/initYearHoliday/setting")
-    @ResponseBody
-    public Object setting(Integer year){
+    public ModelAndView setting(Integer year){
         JSONObject json = new JSONObject();
         try {
             yearHolidayInitService.initSettings(year);
@@ -34,6 +34,6 @@ public class YearHolidayInitAction {
             json.put("success",false);
             json.put("errorMsg","失败:"+e.getMessage());
         }
-        return json;
+        return new ModelAndView("/setting/initYearHoliday");
     }
 }
---
+++ src/main/java/com/vali/service/mail/impl/MailServiceImpl.java
@@ -0,0 +1,10 @@
+package com.vali.service.mail.impl;
+
+import com.vali.service.mail.MailService;
+
+/**
+ * Created by fanshuai on 15/8/25.
+ */
+public class MailServiceImpl implements MailService {
+
+}
